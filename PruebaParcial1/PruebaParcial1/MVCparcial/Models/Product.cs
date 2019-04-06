e1f8da22a66616f14901196571556fa42d1803be
Jose Mario Lami Suárez <jmlami99@gmail.com>
2019-04-05 23:54:45 -0400

    Commit con API creado

---
+++ PruebaParcial1/PruebaParcial1/MVCparcial/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCparcial.Models
+{
+    public enum UniType
+    {
+        Botella, //0
+        Metro,   //1
+        Litro,   //2
+        Caja     //3
+    }
+
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        [Required]
+        public UniType Unit { get; set; }
+        public StatusType Status { get; set; }
+
+    }
+}
---
+++ PruebaParcial1/PruebaParcial1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PruebaParcial1.Startup))]
+namespace PruebaParcial1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
